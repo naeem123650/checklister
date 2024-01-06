eb31da5f001601126a6c48a2f4cf3a918dfec7f3
naeem123650 <naeemmemon60650@gmail.com>
2024-01-06 20:59:53 +0530

    added chirp new and updated wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'website'
+        'website',
+        'last_action_at'
     ];
 
     /**
--- app/View/Composers/MenuComposer.php
+++ app/View/Composers/MenuComposer.php
@@ -3,6 +3,7 @@
 namespace App\View\Composers;
 
 use App\Models\ChecklistGroup;
+use Illuminate\Support\Carbon;
 use Illuminate\View\View;
 
 class MenuComposer
@@ -20,12 +21,20 @@ class MenuComposer
 
         $groups = [];
 
+        $lastActionAt = auth()->user()->last_action_at;
+
+        if(is_null($lastActionAt)) {
+            $lastActionAt = auth()->user()->update(
+               ['last_action_at' => Carbon::now()->addYears(10)]
+            );
+        }
+
         foreach ($menus as $group) {
-            $group['is_new'] = false;
-            $group['is_updated'] = false;
+            $group['is_new'] = Carbon::create($group['created_at'])->greaterThan($lastActionAt);
+            $group['is_updated'] = !($group['is_new']) && Carbon::create($group['updated_at'])->greaterThan($lastActionAt);;
             foreach ($group['checklists'] as &$checklist) {
-                $checklist['is_new'] = false;
-                $checklist['is_updated'] = false;
+                $checklist['is_new'] = Carbon::create($checklist['created_at'])->greaterThan($lastActionAt);
+                $checklist['is_updated'] = !($checklist['is_new']) && Carbon::create($checklist['updated_at'])->greaterThan($lastActionAt);
                 $checklist['tasks'] = 1;
                 $checklist['completed_tasks'] = 0;
             }
